5e0462c5d7cef1c72f94f9b5e1fd9ac8482cab1d
Lucas <lms.snf19@uea.edu.br>
2021-05-27 13:17:35 -0400

    adicionar português

--- chatbot.py
+++ chatbot.py
@@ -4,38 +4,40 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 
 my_bot = ChatBot(
     name="bot",
-    read_only=True,
     logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch']
 )
 
 list_trainer = ListTrainer(my_bot)
-list_trainer.train(['hi there!',
-                    'hi!',
-                    'how do you do?',
-                    'how are you?',
-                    'i\'m cool.',
-                    'fine, you?',
-                    'always cool.',
-                    'i\'m ok',
-                    'glad to hear that.',
-                    'i\'m fine',
-                    'glad to hear that.',
-                    'i feel awesome',
-                    'excellent, glad to hear that.',
-                    'not so good',
-                    'sorry to hear that.',
-                    'what\'s your name?',
-                    'i\'m a bot. ask me a math question, please.'])
+list_trainer.train([
+
+    'Oi?',  # User
+    'Oi, eu sou o chatbot mais curioso dos cinemas, conta pra mim sobre um filme que você assistiu :)',  # bot
+    'Qual o seu nome?',  # User
+    'Conta pra mim, bot feito para conversar sobre filmes.',  # bot
+    'Prazer em te conhecer',  # User
+    'Igualmente, tem algum filme legal para conversar sobre?',  # bot
+    'Quantos anos você tem?',  # User
+    'Eu nasci em 2021.',  # bot
+    'Você gosta de filmes?',  # User
+    'Adoro, me conte um pouco sobre o que você tem assistido.',  # bot
+    'Qual o seu personagem favorito?',  # User
+    'Gandalf, o mago.',  # bot
+    'Você gosta de pipoca?',  # User
+    'Sim, melhor coisa para comer assistindo filmes, apesar de que bot não comem.',  # bot
+    'Hahahaha', 'kkkk',
+    'kkk', 'kkkk',
+])
 
 corpus_trainer = ChatterBotCorpusTrainer(my_bot)
-corpus_trainer.train("chatterbot.corpus.english")
+corpus_trainer.train('chatterbot.corpus.portuguese')
 
+print("Bot: Eu sou Conta pra mim, chatbot dedicado a conversar com você sobre os filmes que assistiu.")
 while True:
     try:
         answer = my_bot.get_response(input("User: "))
-        if float(answer.confidence) > 0.2:
+        if float(answer.confidence) > 0.1:
             print("Bot: ", answer)
         else:
-            print("I can\'t understand you.")
+            print("Não entendo.")
     except(KeyboardInterrupt, EOFError, SystemExit):
         break
