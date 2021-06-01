7853423c3d2628fdb278d51a58985a0515e047ea
Jonatas <jtsdb.snf19@uea.edu.br>
2021-06-01 11:04:41 -0400

    função de resposta e melhora nos diálogos

--- chatbot.py
+++ chatbot.py
@@ -7,25 +7,40 @@ my_bot = ChatBot(
     logic_adapters=['chatterbot.logic.BestMatch'],
 )
 
-list_trainer = ListTrainer(my_bot)
-list_trainer.train([
-    'Oi?',  # User
-    'Oi, eu sou o chatbot mais curioso dos cinemas, conta pra mim sobre um filme que você assistiu :)',  # bot
-    'Qual o seu nome?',  # User
-    'Conta pra mim, bot feito para conversar sobre filmes.',  # bot
-    'Prazer em te conhecer',  # User
-    'Igualmente, tem algum filme legal para conversar sobre?',  # bot
-    'Quantos anos você tem?',  # User
-    'Eu nasci em 2021.',  # bot
-    'Você gosta de filmes?',  # User
-    'Adoro, me conte um pouco sobre o que você tem assistido.',  # bot
-    'Qual o seu personagem favorito?',  # User
-    'Gandalf, o mago.',  # bot
-    'Você gosta de pipoca?',  # User
-    'Sim, melhor coisa para comer assistindo filmes, apesar de que bot não comem.',  # bot
-    'Hahahaha', 'kkkk',
-    'kkk', 'kkkk',
-])
+def answer(msg):
+
+        ans = my_bot.get_response(msg)
+        
+        if msg.lower() == "tchau":
+            return "Tchau tchau, até a próxima :)\n"
+    # respostas com base na confiança
+        # elif float(answer.confidence) > 0.05:
+        #    print("Bot: ", answer)
+        #else:
+        #    print("Não entendo.")
+        else:
+            return ans
+
+
+#list_trainer = ListTrainer(my_bot)
+#list_trainer.train([
+#    'Oi?',  # User
+#    'Oi, eu sou o chatbot mais curioso dos cinemas, conta pra mim sobre um filme que você assistiu :)',  # bot
+#    'Qual o seu nome?',  # User
+#    'Conta pra mim, bot feito para conversar sobre filmes.',  # bot
+#    'Prazer em te conhecer',  # User
+#    'Igualmente, tem algum filme legal para conversar sobre?',  # bot
+#    'Quantos anos você tem?',  # User
+#    'Eu nasci em 2021.',  # bot
+#    'Você gosta de filmes?',  # User
+#    'Adoro, me conte um pouco sobre o que você tem assistido.',  # bot
+#    'Qual o seu personagem favorito?',  # User
+#    'Gandalf, o mago.',  # bot
+#    'Você gosta de pipoca?',  # User
+#    'Sim, melhor coisa para comer assistindo filmes, apesar de que bot não comem.',  # bot
+#    'Hahahaha', 'kkkk',
+#    'kkk', 'kkkk',
+#])
 
 corpus_trainer = ChatterBotCorpusTrainer(my_bot)
 corpus_trainer.train("./Corpus")
@@ -37,18 +52,12 @@ talking = True
 while talking:
     try:
 
-        userInput = input("User: ")
-        answer = my_bot.get_response(userInput)
-        
-        if userInput.lower() == "tchau":
+        userInput = input("User: ").lower()
+        ans = answer(userInput)
+        print("Bot: ", ans)
+
+        if (ans == "Tchau tchau, até a próxima :)\n"):
             talking = False
-            print("Bot: Tchau tchau, até a próxima :)")
-    # respostas com base na confiança
-        # elif float(answer.confidence) > 0.05:
-        #    print("Bot: ", answer)
-        #else:
-        #    print("Não entendo.")
-        else:
-           print("Bot: ", answer)
+        
     except(KeyboardInterrupt, EOFError, SystemExit):
         break
